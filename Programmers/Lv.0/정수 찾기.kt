982019550e3395a11ad4cd4819951d0fb0c74c8e
KIM TAEEUN <snowykte0426@naver.com>
2024-08-29 17:03:43 +0900

    20240829/Programmers Lv.0 정수 찾기

---
+++ Programmers/Lv.0/정수 찾기.kt
@@ -0,0 +1,10 @@
+class Solution {
+    fun solution(num_list: IntArray, n: Int): Int {
+        for (i in num_list.indices) {
+            if (num_list[i] == n) {
+                return 1
+            }
+        }
+        return 0
+    }
+}
