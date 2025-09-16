889ae7cd36e67b91c57c6ee89d109dbcb00b7fdd
Kim Tae Eun <snowykte0426@naver.com>
2025-09-16 11:04:44 +0900

    20250916/Programmers Lv.0 문자열의 앞의 n글자

--- Programmers/Lv.0/문자열의 앞의 n글자.java
+++ Programmers/Lv.0/문자열의 앞의 n글자.java
@@ -1,5 +1,5 @@
 class Solution {
     public String solution(String my_string, int n) {
-        return my_string.split(String.valueOf(my_string.charAt(n)))[0];
+        return my_string.substring(0, n);
     }
 }
