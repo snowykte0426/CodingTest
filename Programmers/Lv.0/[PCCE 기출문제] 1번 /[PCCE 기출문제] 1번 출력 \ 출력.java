fec1aa6d32f023fd762f619fc3d86c5b6dcbe866
Kim Tae Eun <snowykte0426@naver.com>
2025-09-16 10:33:07 +0900

    20250916/20240829/Programmers Lv.0 [PCCE 기출문제] 1번 / 출력

---
+++ Programmers/Lv.0/[PCCE 기출문제] 1번 /[PCCE 기출문제] 1번 출력 \ 출력.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        String msg = ;
+        int val1 = ;
+        String val2 = ;
+
+        System.out.println(msg);
+        System.out.println(val1 + 10);
+        System.out.println(val2 + "10");
+    }
+}
