32bca7b2ec0b076e03e8832153647f752088db45
Kim Tae Eun <snowykte0426@naver.com>
2025-09-16 10:34:11 +0900

    20240829/Programmers Lv.0 flag에 따라 다른 값 반환하기

--- Programmers/Lv.0/[PCCE 기출문제] 1번 출력 \ 출력.java
+++ Programmers/Lv.0/[PCCE 기출문제] 1번 출력 \ 출력.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
-        String msg = ;
-        int val1 = ;
-        String val2 = ;
+        String msg = "Spring is beginning";
+        int val1 = 3;
+        String val2 = "3";
 
         System.out.println(msg);
         System.out.println(val1 + 10);
