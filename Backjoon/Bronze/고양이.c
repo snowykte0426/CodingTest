485218b766d9b99a37df14b26b885b0fe358e786
Kim Tae Eun <snowykte0426@naver.com>
2025-10-11 01:39:34 +0900

    20240319/Bronze V 고양이

---
+++ Backjoon/Bronze/고양이.c
@@ -0,0 +1,13 @@
+#ifndef _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
+#include <cstdio>
+
+int main() {
+	printf("\\    /\\\n");
+	printf(" )  ( ')\n");
+	printf("(  /  ) \n");
+	printf(" \\(__)|\n");
+	return 0;
+}
