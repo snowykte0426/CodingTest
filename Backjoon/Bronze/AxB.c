91e3778bc86b0bc202297646c49390d04c52b8c1
Kim Tae Eun <snowykte0426@naver.com>
2025-10-12 13:56:03 +0900

    20240319/Bronze V AxB

---
+++ Backjoon/Bronze/AxB.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int kkkkk(void);
+
+int main(void) {
+    kkkkk();
+    return 0;
+}
+
+int kkkkk(void) {
+    int n1, n2;
+    scanf("%d %d", &n1, &n2);
+    printf("%d", n1 * n2);
+    return 0;
+}
