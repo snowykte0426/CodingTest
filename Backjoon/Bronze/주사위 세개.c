8e143666017e946c4226140e420f4ee0b9b85d0b
Kim Tae Eun <snowykte0426@naver.com>
2025-10-12 13:03:03 +0900

    20240322/Bronze IV 주사위 세개
    Implement dice game scoring based on three dice values.

---
+++ Backjoon/Bronze/주사위 세개.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void) {
+    int A, B, C;
+    scanf("%d %d %d", &A, &B, &C);
+
+    if (A == B && B == C)
+        printf("%d", 10000 + A * 1000);
+    else if (A == B || B == C)
+        printf("%d", 1000 + B * 100);
+    else if (C == A)
+        printf("%d", 1000 + C * 100);
+    else {
+        if (A > B && A > C)
+            printf("%d", A * 100);
+        else if (B > A && B > C)
+            printf("%d", B * 100);
+        else
+            printf("%d", C * 100);
+    }
+
+    return 0;
+}
