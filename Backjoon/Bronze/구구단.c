367e99c7714596b2686fe61729b0cf07ba6daa74
Kim Tae Eun <snowykte0426@naver.com>
2025-10-12 13:12:53 +0900

    20240325/Bronze V 구구단

---
+++ Backjoon/Bronze/구구단.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+    int number = NULL;
+    scanf("%d", &number);
+    
+    for (int i = 1; i <= 9; i++) {
+        printf("%d * %d = %d\n", number, i, number * i);
+    }
+    
+    return 0;
+}
