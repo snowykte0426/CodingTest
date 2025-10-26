52c01a140b5366dbba072e10cb3e81815d4adb3f
Kim Tae Eun <snowykte0426@naver.com>
2025-10-26 13:15:47 +0900

    20251026/Programmers Lv.0 배열 만들기 6

---
+++ Programmers/Lv.0/배열 만들기 6.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        List<Integer> stk = new ArrayList<>();
+        int i = 0;
+        while (i < arr.length) {
+            if (stk.isEmpty()) {
+                stk.add(arr[i]);
+                i++;
+            } else if (stk.get(stk.size() - 1) == arr[i]) {
+                stk.remove(stk.size() - 1);
+                i++;
+            } else {
+                stk.add(arr[i]);
+                i++;
+            }
+        }
+        if (stk.isEmpty()) {
+            return new int[]{-1};
+        }
+        return stk.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
