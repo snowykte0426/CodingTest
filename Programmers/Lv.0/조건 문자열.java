868eb811c47c530883e00526cd0fe09c969fdc6d
Kim Tae Eun <snowykte0426@naver.com>
2025-10-27 10:30:05 +0900

    20251027/Programmers Lv.0 조건 문자열

---
+++ Programmers/Lv.0/조건 문자열.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(String ineq, String eq, int n, int m) {
+        switch (ineq) {
+            case "<":
+                if (eq.equals("=")) {
+                    return n <= m ? 1 : 0;
+                } else {
+                    return n < m ? 1 : 0;
+                }
+            case ">":
+                if (eq.equals("=")) {
+                    return n >= m ? 1 : 0;
+                } else {
+                    return n > m ? 1 : 0;
+                }
+            default:
+                return 0;
+        }
+    }
+}
